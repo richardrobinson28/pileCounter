aba08c189a3fdddd9f7309debf156c30ba255053
Richard Robinson <richardrobinson28@users.noreply.github.com>
2017-03-06 23:22:50 +0000

    Initial upload of java file

---
+++ pyramid.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class pyramid {
+	public static void main(String args[]) {
+		// Declares input
+		Scanner input = new Scanner(System.in);
+
+		// Declares variables
+		int triangle, add, pile;
+		double coin, multiply;
+
+		// Output asks length of longest side of triangle and allows user input to initialise 
+		System.out.println("How long is the longest line of triangle?");
+		triangle = input.nextInt();
+
+		// Output asks what coin is being used and allows user input
+		System.out.println("\nWhat coin are you using?");
+		coin = input.nextDouble();
+
+		// Output asks for how many coins are in each pile and allows user input
+		System.out.println("\nHow many coins are in each pile?");
+		pile = input.nextInt();
+
+		// Calls methods and initialises variables
+		add = addition(triangle); // uses addition method to work out sum of the rows of a triangle
+		multiply = multiplication(coin, pile, add); // uses multiplication method to work out sum of coins in all piles within triangle 
+
+		// Print out functions
+		System.out.println("\n" + add + " piles"); // Prints out sum of rows of triangle
+		System.out.println("£" + multiply); // Prints out sum of coins
+
+		// Closes input
+		input.close();
+	}
+
+	private static int addition(int a) {
+		// Declares and initialises the variable b
+		int b = 0;
+
+		// For loop that loops through triangle from longest to shortest triangle line and adds each line to variable b
+		for (int i = a; i > 0; i--) {
+			b += i;
+		}
+
+		// Returns value of b after for loop
+		return b;
+	}
+
+	private static double multiplication(double a, int b, int c) {
+		// Returns total of a multiplied by b multiplied by c
+		return (a * b * c);
+	}
+}
